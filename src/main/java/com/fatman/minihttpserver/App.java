1846a886f1e03886ee783607b65b635be7075fe6
fatmanhappycode <1943279276@qq.com>
2020-04-06 23:46:23 +0800

    清理不必要代码

--- src/main/java/com/fatman/minihttpserver/App.java
+++ src/main/java/com/fatman/minihttpserver/App.java
@@ -6,11 +6,14 @@ import com.fatman.minihttpserver.httpserver.*;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
-public class RestServerSample {
+/**
+ * @author 肥宅快乐码
+ */
+public class App {
 
-    public static void serverStart() throws IOException {
+    private static void serverStart() throws IOException {
+        //监听端口8080
         HttpServer httpserver = HttpServerProvider.createHttpServer(new InetSocketAddress(8080), 0);
-        //监听端口8080,  
 
         httpserver.createContext("/RestSample");
         httpserver.start();
